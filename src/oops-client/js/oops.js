6d1da53c79ca0446932aaddcd0ea710d747a0742
Christophe Beveraggi <beve@croll.fr>
2013-09-21 23:33:28 +0200

    - Mobile interface

--- src/oops-client/js/oops.js
+++ src/oops-client/js/oops.js
@@ -1,5 +1,3 @@
-
-
 var tpl;
 define = function(jade, cb) {
     tpl = cb();
@@ -11,11 +9,15 @@ var currentPopup = null;
 var plot = null;
 
 oops.loadTemplate = function(name) {
-    console.log($("#oopscontent"));
-    $("#oopscontent").html(tpl(name));
-        $('#oopscontent').trigger('create');
+    $("#content").html(tpl(name));
+        $('#content').trigger('create');
 }
 
+    $(window).on("pagechange", function(event, ui){
+         if ($.mobile.activePage.attr('id') == 'mappage') {
+             map.invalidateSize(false);
+         }
+    });
  
 oops.checkPlace = function(lat, lon) {
     $.ajax({
